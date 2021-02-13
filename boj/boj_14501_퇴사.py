4d994e128d85cc9984075eea3e929c414e48b4d8
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-02-13 18:58:04 +0900

    [BOJ] DP #1. 2차원 배열, #2. 1차원 배열(역순) - 복습하자
    - 두 방법 : 메모리 ==

--- boj/boj_14501_퇴사.py
+++ boj/boj_14501_퇴사.py
@@ -1,3 +1,4 @@
+## 1. DP 2차원 배열
 import sys
 input = sys.stdin.readline
 
@@ -27,3 +28,23 @@ for i in range(n):
       dp[i][k] = dp[idx][k]  
 
 print(max(result))
+
+## 2. DP 1차원 배열 역순으로 계산
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+table = []
+for i in range(n):
+  a, b = map(int, input().split())
+  table.append((a, b))
+
+dp = [0] * (n + 1) # 다이나믹 프로그래밍
+
+for i in range(n - 1, -1, -1):
+  if table[i][0] + i <= n: # 일수가 넘어가지 않을 때 
+    dp[i] = max(table[i][1] + dp[i + table[i][0]], dp[i + 1])
+  else:
+    dp[i] = dp[i + 1]
+
+print(dp[0])
