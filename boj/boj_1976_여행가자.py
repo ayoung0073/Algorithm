f46a9b6ade3febf5ac78072e12edd7e275ec5b24
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-02-24 14:54:25 +0900

    [BOJ] union-find, 플로이드워셜 - 복습하기
    - 플로이드 워셜 알고리즘 추가

--- boj/boj_1976_여행가자.py
+++ boj/boj_1976_여행가자.py
@@ -1,3 +1,5 @@
+## 1. union-find
+
 import sys  
 input = sys.stdin.readline
 
@@ -50,3 +52,37 @@ for p in plan:
 
 # 모든 여행 경로에 대해 root가 같으면 YES
 print('YES' if check else 'NO')
+
+
+## 2. 플로이드-워셜
+n = int(input())  # 총 도시의 수
+m = int(input())  # 여행 계획에 속한 도시들의 수
+graph = [list(map(int, input().split())) for _ in range(n)]
+plan = list(map(int, input().split()))
+
+# 플로이드-워셜 알고리즘을 통해 모든 노드 간의 최소 거리를 구한다.
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 0 and i != j:  # 길이 없는 경우
+            graph[i][j] = 1e9  # 10억으로 대입
+
+# 최소 거리 갱신
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+# 여행 여부 확인
+check = True
+prev = plan[0] - 1
+for i in range(1, m):
+    if graph[prev][plan[i] - 1] >= 1e9:  # 길이 없는 경우
+        check = False
+        break
+    else:
+        prev = plan[i] - 1
+
+if check:
+    print("YES")
+else:
+    print("NO")
