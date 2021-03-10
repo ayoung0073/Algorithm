468ee6bdfce7538c9eabdc99b07228dc4d94515e
ayoung0073 <ayong0310@naver.com>
2021-03-11 00:31:20 +0900

    2주차 : 기조데이터구조-나선형

---
+++ 소스코드/2주차_기초데이터구조/02_행렬나선형/spiral.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#define SIZE 100
+
+void spiral(int A[][SIZE], int n, int m)
+{
+    int left = 0, right = m - 1;
+    int top = 0, bottom = n - 1;
+    
+    int value = 1;
+    
+    int i = 0, j = 0;
+    
+    while (left <= right && top <= bottom)
+    {
+        for (j = left; j <= right; j++)
+            A[top][j] = value++;
+        top++;
+        
+        if (top <= bottom)
+        {
+            for(i = top; i <= bottom; i++)
+                A[i][right] = value++;
+            right--;
+        }
+        
+        if (left <= right)
+        {
+            for(j = right; j >= left; j--)
+                A[bottom][j] = value++;
+            bottom--;
+        }
+        
+        if(top <= bottom)
+        {
+            for (i = bottom; i >= top; i--)
+                A[i][left] = value++;
+            left++;
+        }
+    }
+
+}
+
+void printArray(int A[][SIZE], int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            printf("%2d ", A[i][j]);
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int A[SIZE][SIZE] = {0};
+    int n, m;
+    scanf("%d %d", &n, &m);
+    spiral(A, n, m);
+    printArray(A, n, m);
+
+    return 0;
+}
+
+/*
+5
+4 5                                                                                                                                                                                
+ 1  2  3  4  5                                                                                                                                                                     
+14 15 16 17  6                                                                                                                                                                     
+13 20 19 18  7                                                                                                                                                                     
+12 11 10  9  8   
+*/
