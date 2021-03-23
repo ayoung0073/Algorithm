4da2edacd7eb1f08e290d851c679ca508538262f
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-03-24 00:19:20 +0900

    [BOJ] DP - 꼭 복습하기
    - 66%
    - 못 품 .... ... 
    - 꼭 복습....

---
+++ boj_15989_123더하기4.py
@@ -0,0 +1,29 @@
+## 15989. 1, 2, 3, 더하기 4
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+arr = []
+
+for _ in range(t):
+  arr.append(int(input()))
+
+dp = [[0, 0, 0, 0] for _ in range(10001)]
+
+dp[1][1] = 1
+
+dp[2][1] = 1
+dp[2][2] = 1
+
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
+
+for i in range(4, 10001):
+  dp[i][1] = dp[i - 1][1]
+  dp[i][2] = dp[i - 2][1] + dp[i - 2][2]
+  dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3]
+
+for i in arr:
+  print(sum(dp[i]))
