c78bd8fc49abac0afc66dfbdeed18bca91444e7d
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-04-08 10:36:58 +0900

    [BOJ] 9470 복습
    - 성공
    - 위상정렬 떠올렸다

--- boj/boj_9470_Strahler순서.py
+++ boj/boj_9470_Strahler순서.py
@@ -1,47 +1,39 @@
-## 9470. Strachler 순서
-
 import sys
-from collections import deque
+import heapq
 
 input = sys.stdin.readline
-
 t = int(input())
-
 for _ in range(t):
   k, m, p = map(int, input().split())
-
+  parent = [0] * (m + 1)
+  arr = [0] * (m + 1)
   graph = [[] for _ in range(m + 1)]
-  indegree = [0] * (m + 1)
-
-  order = [0] * (m + 1)
-  order_max = [[0, 0] for _ in range(m + 1)]
+  dp = [[0] * 1001 for _ in range(1001)]
 
   for _ in range(p):
     a, b = map(int, input().split())
     graph[a].append(b)
-    indegree[b] += 1
+    parent[b] += 1
 
-  q = deque()
-  for i in range(1, m + 1):
-    if indegree[i] == 0:
-      q.append(i)
+  q = []
 
+  for i in range(1, len(parent)):
+    if parent[i] == 0: 
+      heapq.heappush(q, (1, i))
+      arr[i] = 1
+  
   while q:
-    num = q.popleft()
-    if order_max[num][1] == 1:
-      order[num] = order_max[num][0]
-    else:
-      order[num] = order_max[num][0] + 1
-    
-    for i in graph[num]:
-      indegree[i] -= 1
-      if indegree[i] == 0:
-        q.append(i)
-      
-      if order_max[i][0] < order[num]:
-        order_max[i][0] = order[num]
-        order_max[i][1] = 1
-      elif order_max[i][0] == order[num]:
-        order_max[i][1] += 1
+    cnt, now = heapq.heappop(q)
+    for i in graph[now]:
+      parent[i] -= 1
 
-  print(k, order[m])
+      if dp[i][1] != cnt:
+        dp[i][1] = max(dp[i][1], cnt)
+      elif dp[i][1] == cnt:
+        dp[i][2] = max(dp[i][2], cnt)
+
+      if parent[i] == 0: 
+        arr[i] = max(dp[i][2] + 1, dp[i][1])
+        heapq.heappush(q, (arr[i], i))
+
+  print(k, arr[m])
