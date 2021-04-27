dee96a2d06951dda820aa652aa3fdcf8d1ddf4a7
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-04-27 21:53:26 +0900

    [KAKAO] eval 함수 적용
    - arr + [] == arr[:]

--- programmers/수식_최대화.py
+++ programmers/수식_최대화.py
@@ -18,23 +18,37 @@ def solution(expression):
     prior_list = list(permutations(['+', '-', '*'], 3))
 
     for prior in prior_list:
-        cal = save_cal + []
-        val = save_val + []
+        cal = save_cal[:] # == save_cal + []
+        val = save_val[:]
         for i in prior:
             while i in cal:
                 idx = cal.index(i)
-                print(idx)
                 del cal [idx] 
-                print(val)
-                val_1 = int(val[idx])
-                val_2 = int(val[idx + 1])
+                
+                val_1 = val[idx]
+                val_2 = val[idx + 1]
                 del val[idx:idx + 2] 
-                if i == '+':
-                    val.insert(idx, val_1 + val_2)
-                elif i == '-':
-                    val.insert(idx, val_1 - val_2)
-                elif i == '*':
-                    val.insert(idx, val_1 * val_2)
-                    
-        answer = max(answer, abs(val[0]))
+                
+                eval(val_1 + i + val_2)
+                val.insert(idx, str(eval(val_1 + i + val_2)))
+                
+        answer = max(answer, abs(int(val[0])))
     return answer
+
+'''
+100 200 300 500 20
+- * - +
+
+첫번째 +(3) : 500 + 20 = 520 > 500, 20(index 3, 4) 제거 > index 3에 520 추가(arr.insert(3, 520)
+
+100 200 300 520
+
+두번째 -(0) : 100 - 200 = -100 > 100, 200(index 0, 1) 제거 > arr.insert(0, -100)
+
+(연산자도 pop해야할 듯 하다..)
+
+-100 300 520
+
+세번째 -(1) : 300 - 520 = -220
+
+'''
