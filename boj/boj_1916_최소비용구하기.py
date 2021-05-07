2b9e04425fdb9cbb288f4ed007b91b9657951456
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-05-07 12:57:39 +0900

    [BOJ] 다익스트라 복습

--- boj/boj_1916_최소비용구하기.py
+++ boj/boj_1916_최소비용구하기.py
@@ -1,35 +1,34 @@
+## 최소 비용 구하기
 import sys
 import heapq
 
 input = sys.stdin.readline
 
+n = int(input()) # 도시 개수
+m = int(input()) # 버스 개수
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+  s, e, cost = map(int, input().split())
+  graph[s].append((cost, e)) # 출발지 s에서 e까지의 비용
+
 INF = int(1e9)
-
-n = int(input()) # 도시의 개수
-m = int(input()) # 버스의 개수
-
-graph = [[] for i in range(n)] 
-# 튜플로 추가할 것
-
-for i in range(m):
-  a, b, c = map(int, input().split())
-  graph[a - 1].append((b - 1, c))
-  # 출발도시에 (도착도시, 비용) 추가
-
-distance = [INF] * n
+distance = [INF] * (n + 1)
 start, end = map(int, input().split())
-# 출발도시, 도착 도시
-q = [] # 힙 큐
-heapq.heappush(q, (0, start - 1))
-# 0: 거리, start: 노드 번호
-while q:
-  dist, now = heapq.heappop(q)
-  if dist > distance[now]: # 이미 처리된 경우 pass
-    continue
-  for i in graph[now]:
-    cost = dist + i[1] # 현재 선택된 노드에서의 거리 구하기
-    if cost < distance[i[0]]: # distance 갱신 조건
-      distance[i[0]] = cost
-      heapq.heappush(q, (cost, i[0]))
 
-print(distance[end - 1])
+def dijkstra(start):
+  q = []
+  distance[start] = 0
+  heapq.heappush(q, (0, start))
+
+  while q:
+    cost, now = heapq.heappop(q)
+    if distance[now] < cost:
+      continue
+    for i in graph[now]:
+      if distance[i[1]] > i[0] + cost: # 갱신 조건
+        distance[i[1]] = i[0] + cost
+        heapq.heappush(q, (distance[i[1]], i[1]))
+
+dijkstra(start)
+print(distance[end])
