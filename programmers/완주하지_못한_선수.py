879069e84df878608397ecc30a14ae1b880c62fd
MoonAyoung <69340410+ayoung0073@users.noreply.github.com>
2021-05-19 23:50:14 +0900

    [코테 연습] 딕셔너리 이용

--- programmers/완주하지_못한_선수.py
+++ programmers/완주하지_못한_선수.py
@@ -1,3 +1,4 @@
+# 정렬 이용
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -7,3 +8,19 @@ def solution(participant, completion):
             return participant[i]
         
     return participant[len(participant) - 1]
+
+# dictionary 이용
+def solution(participant, completion):
+    dic = {}
+    for i in participant:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+
+    for i in completion:
+        dic[i] -= 1
+
+    for i in participant:
+        if dic[i] == 1:
+            return i
