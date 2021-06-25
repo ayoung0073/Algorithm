509dcee94008bcfd94c9aa2446d5a3be4b8a1524
MoonAyoung <ayong0310@naver.com>
2021-06-25 10:39:25 +0900

    [L2] 정렬 복습
    - 성공
    - lambda 다시 익숙해지기,,
    - map 함수 떠올리자

--- programmers/가장큰수.py
+++ programmers/가장큰수.py
@@ -1,4 +1,6 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*4, reverse=True)
-    return str(int(''.join(numbers)))
+    str_numbers = []
+    for number in numbers:
+        str_numbers.append(str(number)) # numbers = list(map(str, numbers))
+    str_numbers.sort(key=lambda x: x * 5, reverse=True)
+    return str(int("".join(str_numbers)))
