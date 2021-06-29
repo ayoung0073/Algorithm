ea92699a1f757d998fb93c78dfba720ecce011be
MoonAyoung <ayong0310@naver.com>
2021-06-29 11:24:25 +0900

    [BOJ] 12851 복습

--- boj/boj_12851_숨바꼭질.py
+++ boj/boj_12851_숨바꼭질.py
@@ -1,39 +1,39 @@
-## 12851. 숨바꼭질
+import sys  
 import heapq
-n, k = map(int, input().split())
 
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+q = []
+heapq.heappush(q, (0, n))
 INF = int(1e9)
 dp = [INF for _ in range(100001)]
+min_cnt = INF
+answer_cnt = 0
 
-q = []
+while q: 
+  cnt, n = heapq.heappop(q)
 
-heapq.heappush(q, (0, n))
-ans = INF
-cnt = 0
-while q:
-  sec, now = heapq.heappop(q)
-  
-  if sec > ans:
-    break
-    
-  if now == k:
-    if ans == INF:
-      ans = sec
-      cnt += 1
-    elif ans == sec:
-      cnt += 1
+  if n == k:
+    if min_cnt == INF:
+      min_cnt = cnt
+      answer_cnt = 1
+    elif cnt == min_cnt:
+      answer_cnt += 1
     else:
       break
-  
-  if now - 1 >= 0 and dp[now - 1] >= sec + 1:
-    dp[now - 1] = sec + 1
-    heapq.heappush(q, (sec + 1, now - 1))
-  if now + 1 <= 100000 and dp[now + 1] >= sec + 1:
-    dp[now + 1] = sec + 1
-    heapq.heappush(q, (sec + 1, now + 1))
-  if now * 2 <= 100000 and dp[now * 2] >= sec + 1:
-    dp[now * 2] = sec + 1
-    heapq.heappush(q, (sec + 1, now * 2))
 
-print(ans)
-print(cnt)
+  if n - 1 >= 0 and dp[n - 1] >= cnt + 1:
+    dp[n - 1] = cnt + 1
+    heapq.heappush(q, (cnt + 1, n - 1))
+
+  if n + 1 <= 100000 and dp[n + 1] >= cnt + 1:
+    dp[n + 1] = cnt + 1
+    heapq.heappush(q, (cnt + 1, n + 1))
+
+  if n * 2 <= 100000 and dp[n * 2] >= cnt + 1:
+    dp[n * 2] = cnt + 1
+    heapq.heappush(q, (cnt + 1, n * 2))
+
+print(min_cnt)
+print(answer_cnt)
