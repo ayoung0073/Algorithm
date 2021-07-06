764b25f9190874066cc2281450dbeaf01e0fc698
MoonAyoung <ayong0310@naver.com>
2021-07-06 09:59:10 +0900

    [BOJ] 9576 복습

--- boj/boj_9576_책나눠주기.py
+++ boj/boj_9576_책나눠주기.py
@@ -1,20 +1,24 @@
-t = int(input())
+import sys
 
+input = sys.stdin.readline
+
+t = int(input())
 for _ in range(t):
-  result = 0
   n, m = map(int, input().split())
   books = [False] * (n + 1)
-  reqs = []
+  requests = []
   for _ in range(m):
-    reqs.append(list(map(int, input().split())))
+    a, b = map(int, input().split())
+    requests.append((a, b))
 
-  reqs.sort(key = lambda x:x[1]) # b를 기준으로 오름차순
-  
-  while reqs:
-    a, b = reqs.pop(0)
+  requests.sort(key=lambda x:x[1]) # b번을 기준으로 오름차순
+
+  cnt = 0
+  # 앞에서부터 search하여 값이 False이면 나눠준다.
+  for a, b in requests:
     for i in range(a, b + 1):
-      if not books[i]: # False이면, 나눠줄 수 있음
-        result += 1
+      if not books[i]:
         books[i] = True
+        cnt += 1
         break
-  print(result)
+  print(cnt)
