6a94f67ccc6733ab8cd5c59e821a1ebdc40845e3
MoonAyoung <ayong0310@naver.com>
2021-07-22 10:43:33 +0900

    [BOJ] 1946 복습
    - 메모리양 줄음
    - 또 처음에 시간초과 나옴

--- boj/boj_1946_신입사원.py
+++ boj/boj_1946_신입사원.py
@@ -1,25 +1,19 @@
-## 1946. 신입 사원
 import sys
-
 input = sys.stdin.readline
-t = int(input()) # 테스트 케이스의 개수
 
-scores = [[] for i in range(t)]
-for i in range(t):
-  n = int(input()) # 지원자 수
-  for j in range(n):
-    a, b = map(int, input().split())
-    scores[i].append((a, b))
+t = int(input())
+for _ in range(t):
+  ans = 1
+  n = int(input())
+  score = []
+  for _ in range(n):
+    score.append(list(map(int, input().split())))
 
-  scores[i].sort(key = lambda x: x[0]) # 서류 심사 순위를 기준으로 오름차순 정렬
+  score.sort(key=lambda x : x[0])
+  comp = score[0][1]
+  for i in range(1, n):
+    if score[i][1] < comp:
+      comp = score[i][1]
+      ans += 1
 
-# 나보다 서류 성적 좋은 애들 중, 가장 순위가 높은 것보다 높으면 될 듯하다 max값 하나 지정하자
-
-for i in range(t):
-  max_score = len(scores[i]) + 1
-  count = 0
-  for score in scores[i]:
-    if score[1] < max_score: # 더 높은 순위일 때 통과함
-      count += 1
-      max_score = min(max_score, score[1])
-  print(count)
+  print(ans)
