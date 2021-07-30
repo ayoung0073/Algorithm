0380f0577a67c4cc5abcf514c04c16b089bdc778
MoonAyoung <ayong0310@naver.com>
2021-07-30 12:00:12 +0900

    [BOJ] 그리디 복습

--- boj/boj_11501_주식.py
+++ boj/boj_11501_주식.py
@@ -14,3 +14,29 @@ for _ in range(t):
     else:
       ans += (max_val - arr[i])
   print(ans)
+
+  
+### 21.07.30  풀이 추가
+  
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  arr = list(map(int, input().split())) # 날 별 주가 
+
+  ans = 0
+  max_idx = n - 1 
+  
+  for i in range(n - 2, -1, -1): 
+    if arr[i] > arr[max_idx]:
+      for j in range(i + 1, max_idx):
+        ans += arr[max_idx] - arr[j]
+      max_idx = i
+
+  if max_idx > 0:
+    for i in range(0, max_idx):
+      ans += arr[max_idx] - arr[i]
+  
+  print(ans)
