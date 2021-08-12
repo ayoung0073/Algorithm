ef796f4fd82c679c462e9f6abc5278a1f1870950
MoonAyoung <ayong0310@naver.com>
2021-08-12 11:12:14 +0900

    [BOJ] 이분 탐색 LIS 풀이 추가

--- boj/boj_18353_병사배치하기.py
+++ boj/boj_18353_병사배치하기.py
@@ -1,3 +1,4 @@
+### DP.
 n = int(input())
 power = list(map(int, input().split()))
 power.reverse() # LIS 알고리즘을 위해 역순
@@ -10,3 +11,26 @@ for i in range(1, n):
       dp[i] = max(dp[i], dp[j] + 1)
 
 print(n - max(dp))
+
+### 이분 탐색.
+from bisect import bisect_left
+import sys 
+input = sys.stdin.readline 
+
+n = int(input())
+fight = list(map(int, input().split()))
+# 이분 탐색을 위해 배열을 뒤집는다. (오름차순으로 save 배열에 저장할 것)
+fight.reverse()
+## 열외해야 하는 병사의 수를 출력해야 한다.
+save = [fight[0]]
+
+# save에 최대한 작은 값을 넣어야 한다. 그래야 다른 수들이 많이 들어올 수 있다.
+# 크다면 append
+for i in fight[1:]:
+  if save[-1] < i:
+    save.append(i)
+  else:
+    idx = bisect_left(save, i)
+    save[idx] = i
+
+print(n - len(save))
