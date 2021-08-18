9e40c3466019426004a40bf68445e84c5e8ffcad
MoonAyoung <ayong0310@naver.com>
2021-08-18 11:23:22 +0900

    [BOJ] 복습

--- boj/boj_1946_신입사원.py
+++ boj/boj_1946_신입사원.py
@@ -1,19 +1,23 @@
-import sys
-input = sys.stdin.readline
+## 신입 사원
+import sys 
+input = sys.stdin.readline 
 
 t = int(input())
 for _ in range(t):
-  ans = 1
   n = int(input())
   score = []
   for _ in range(n):
     score.append(list(map(int, input().split())))
 
-  score.sort(key=lambda x : x[0])
-  comp = score[0][1]
-  for i in range(1, n):
-    if score[i][1] < comp:
-      comp = score[i][1]
+  ans = 1
+  # 순위 오름차순 정렬
+  score.sort(key=lambda x:x[0]) 
+  max_interview_score = score[0][1]
+  for arr in score[1:]:
+    if arr[1] < max_interview_score: # 더 높은 순위(합격 가능)
+      max_interview_score = arr[1]
       ans += 1
-
+  
   print(ans)
+
+
